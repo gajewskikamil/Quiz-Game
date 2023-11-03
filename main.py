f4b25d6d29e7cff0edbebdb0adbe492c998cdde9
gajewskikamil <gajewskikamil.poczta@gmail.com>
2023-11-03 08:22:38 +0100

    Commit

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+from data import level
 from question_model import Question
-from data import question_data
 from quiz_brain import QuizBrain
 
+user_level = ""
+while user_level not in level:
+    user_level = input("Chose Quiz difficulty (easy, medium, hard)")
 question_bank = []
-for qa in question_data:
-    question_bank.append(Question(qa["text"], qa["answer"]))
+for qa in level[user_level]:
+    question_bank.append(Question(qa["question"], qa["correct_answer"]))
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
-
+while quiz.still_has_questions():
+    quiz.next_question()
--- quiz_brain.py
+++ quiz_brain.py
@@ -2,8 +2,26 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
     def next_question(self):
-        num = self.question_number
+        current_question = self.question_list[self.question_number]
         self.question_number += 1
-        return input(f"Q{num + 1}: {self.question_list[num].text} (True/False) ")
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False) ").title()
+        self.check_answer(user_answer, current_question.answer)
+
+    def still_has_questions(self):
+        if self.question_number < len(self.question_list):
+            return True
+        else:
+            print("You've completed the quiz!")
+            print(f"You final score was: {self.score}/{self.question_number}")
+
+    def check_answer(self, user_a, correct_a):
+        if user_a == correct_a:
+            print("Correct!")
+            self.score += 1
+        else:
+            print("that's wrong.")
+        print(f"The Correct answer was: {correct_a}")
+        print(f"Your current score is: {self.score}/{self.question_number}\n\n")
